b765b0069ff9f1e7100f2933637ce88d0b662e08
Joshua Gelbard <jgelbard@e1z4r15p4.42.us.org>
2018-06-07 17:44:46 -0700

    Make an ast_node and print its tokens

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -1,7 +1,34 @@
 #include "ft_sh.h"
 
+#define RVAL_INITIAL -1
+#define CTYPE_INITIAL CMD + 1
+
+void	print_tokens(char **tokens)
+{
+	printf("%s", "[");
+	int i = 0;
+	while (tokens[i])
+	{
+		printf("%d: '%s'%s", i, tokens[i], tokens[i + 1] ? ", " : "]");
+		++i;
+	}
+}
+
+t_ast	*ast_node()
+{
+	t_ast *ast = ft_memalloc(sizeof(*ast));
+	ast->rval = RVAL_INITIAL;
+	ast->ctype = CTYPE_INITIAL;
+	return (ast);
+}
+
 int	main(void)
 {
 	puts("hi");
+	t_ast *ast = ast_node();
+	char *tmp[] = { "a", "na", NULL };
+	ast->tokens = tmp;
+	print_tokens(ast->tokens);
+	puts("");
 	return (0);
 }
