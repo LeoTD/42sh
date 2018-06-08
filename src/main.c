e619427078362aca73511ed40c85040730bbe586
regien <gerardomalpartidavela@gmail.com>
2018-06-08 01:09:17 -0700

    getting latest update header

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ltanenba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 12:24:30 by ltanenba          #+#    #+#             */
-/*   Updated: 2018/06/06 21:46:51 by ckrommen         ###   ########.fr       */
+/*   Updated: 2018/06/08 01:07:02 by gmalpart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 int			main(int argc, char **argv)
 {
 	char **args;
+	char **second;
 
 	if (argc == 2)
 	{
-		ft_printf("%s", "Hello 42sh!\n");
+//		ft_printf("%s", "Hello 42sh!\n");
 		args = split_args(argv[1]);
+		printf("end of old one\n");
+		second =  tokenizer(argv[1]);
 	}
 //	shell_init();
 //	prompt(g_shell);
---
+++ src/tokenizer.c
@@ -0,0 +1,12 @@
+#include "ft_sh.h"
+
+char	**tokenizer(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		if()
+	}
+}
