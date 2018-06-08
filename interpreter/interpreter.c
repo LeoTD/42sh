5c901fed2ede113960cf60e6b5cf62d939638429
Joshua Gelbard <jgelbard@e1z4r15p4.42.us.org>
2018-06-08 16:28:26 -0700

    Change printf( to fprintf(stderr,

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -28,14 +28,14 @@ void	print_tokens(char **tokens)
 {
 	if (!tokens)
 	{
-		printf("(null)");
+		fprintf(stderr, "(null)");
 		return;
 	}
-	printf("%s", "[");
+	fprintf(stderr, "%s", "[");
 	int i = 0;
 	while (tokens[i])
 	{
-		printf("'%s'%s", tokens[i], tokens[i + 1] ? ", " : "]");
+		fprintf(stderr, "'%s'%s", tokens[i], tokens[i + 1] ? ", " : "]");
 		++i;
 	}
 }
@@ -43,7 +43,7 @@ void	print_tokens(char **tokens)
 void	print_node(t_ast *a)
 {
 	if (!a)
-		printf("%s", NULL);
+		fprintf(stderr, "%s", NULL);
 	else
 		print_tokens(a->tokens);
 }
@@ -58,13 +58,13 @@ void	examine_tree(t_ast *a)
 {
 	if (a == NULL)
 		return ;
-	print_tokens(a->tokens);printf(": My type is '%s'!", g_cmd_names[a->type]);
+	print_tokens(a->tokens);fprintf(stderr, ": My type is '%s'!", g_cmd_names[a->type]);
 	if (a->lchild == NULL && a->rchild == NULL)
-		printf("\n\tNo children. :(\n");
+		fprintf(stderr, "\n\tNo children. :(\n");
 	else
 	{
-		printf("\n\tMy left child is "); print_node(a->lchild);
-		printf("\n\tand my right child is "); print_node(a->rchild); puts(".");
+		fprintf(stderr, "\n\tMy left child is "); print_node(a->lchild);
+		fprintf(stderr, "\n\tand my right child is "); print_node(a->rchild); fputs(".", stderr);
 	}
 	examine_tree(a->lchild);
 	examine_tree(a->rchild);
