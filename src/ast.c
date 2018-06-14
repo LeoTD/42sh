44c04db146844b7ac1aa77ddf03454a74c0cc3d0
Joshua Gelbard <jgelbard@e1z4r13p3.42.us.org>
2018-06-13 23:08:49 -0700

    call me an exterminator from all these dead bugs

--- src/ast.c
+++ src/ast.c
@@ -11,12 +11,26 @@ char	*g_cmd_symbols[] =
 	[CMD] = NULL
 };
 
+char	**fetch_tokens(char **args)
+{
+  char	**ptr;
+  int	i;
+
+  i = 0;
+  if ((ptr = (char **)malloc(sizeof(char *) * arr_length(args))))
+    return (NULL);
+  while (*(args) && !_op(*(args[0])))
+    ptr[i++] = ft_strdup(*(args));
+  ptr[i] = NULL;
+  return (ptr);
+}
+
 t_ast	*parsed_ast_node(char **args, int *tokens, int hp)
 {
 	t_ast *a;
 
 	a = ft_memalloc(sizeof(*a));
-	a->tokens = args + hp;
+	a->tokens = args;
 	a->type = tokens[hp];
 	a->ok = 0;
 	a->lchild = NULL;
@@ -49,7 +63,10 @@ void	print_tree(t_ast *ast, int i)
 {
 	if (ast)
 	{
-		printf("type = %s level %d\n", *(ast->tokens), i);
+	  		printf("type = %s level %d\n\n", *(ast->tokens), i);
+		for (int x = 0; ast->tokens[x]; x++)
+		  printf("token %d == %s\n", x, ast->tokens[x]);
+		ft_putendl("");
 		i += 1;
 		if (ast->lchild)
 		{
@@ -68,7 +85,7 @@ void	create_tree(char **args, int *tokens, t_ast **head, int hp)
 
 	if (hp != 0)
 	{
-		*head = parsed_ast_node(args, tokens, hp);
+		*head = parsed_ast_node(args+hp, tokens, hp);
 		ast = *head;
 		tokens[hp] = 0;
 		if (args+hp)
@@ -78,7 +95,6 @@ void	create_tree(char **args, int *tokens, t_ast **head, int hp)
 	}
 	else
 		*head = parsed_ast_node(args, tokens, hp);
-//	print_tree(head);
 //	printf("%s \n%s \n%s\n", head->tokens[0], head->lchild->tokens[0], head->rchild->tokens[0]);
 //	return (head);
 }
