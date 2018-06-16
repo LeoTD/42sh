840fc90fa482b137c8e892a20de5477f2e67aa58
Leonardo Tanenbaum-diaz <ltanenba@e1z1r6p18.42.us.org>
2018-06-15 22:18:36 -0700

    bleh.

--- src/builtins.c
+++ src/builtins.c
@@ -25,3 +25,58 @@ int			ftsh_exit(char **args)
 	args = 0;
 	return (0);
 }
+
+int			ftsh_echo(char **args)
+{
+	int			i;
+
+	i = 0;
+	while (args[++i] != NULL)
+	{
+		if (i > 1)
+			ft_putchar(' ');
+		ft_putstr(args[i]);
+	}
+	return (0);
+}
+
+/*
+** Usage:
+** setenv VAR [VALUE]
+*/
+
+int			ftsh_setenv(char **args)
+{
+	int			i;
+	int			nlen;
+
+	if (args[1] == NULL)
+		return (ftsh_env(args));
+	nlen = (int)ft_strlen(args[1]);
+	i = -1;
+	while (environ[++i] != NULL)
+		if (!ft_strncmp(environ[i], args[1], nlen))
+			if (environ[i][nlen] == '=')
+				break ;
+	if (environ[i] == NULL)
+		;//Add new var str "VAR=args[2]"
+	else
+	{
+		//environ[i] = 
+	}
+}
+int			ftsh_unsetenv(char **args)
+{
+
+}
+
+int			ftsh_env(char **args)
+{
+	int			i;
+
+	i = -1;
+	while (environ[++i] != NULL)
+		ft_putendl(environ[i]);
+	args = 0;
+	return (0);
+}
