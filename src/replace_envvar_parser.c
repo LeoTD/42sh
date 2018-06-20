ae79540228fc97b90bb623ae7ec7fa53d0858c23
Joshua Gelbard <jgelbard@e1z4r15p4.42.us.org>
2018-06-20 02:26:47 -0700

    Add forgotten file

---
+++ src/replace_envvar_parser.c
@@ -0,0 +1,57 @@
+#include "ft_sh.h"
+
+char	*ft_get_env(char *name, char **env)
+{
+	int		i;
+	size_t	len;
+
+	i = -1;
+	len = ft_strlen(name);
+	while (env[++i])
+	{
+		if (!(ft_strncmp(env[i], name, len))
+				&& env[i][len] == '=')
+			return (ft_strchr(env[i], '=') + 1);
+	}
+	return (NULL);
+}
+
+void	add_env_entry(char *entry)
+{
+	size_t	envsize;
+	char	**new_env;
+	int		i;
+
+	envsize = arr_length(g_environ);
+	new_env = ft_memalloc((envsize + 2) * sizeof(*new_env));
+	i = 0;
+	while (g_environ[i])
+	{
+		new_env[i] = g_environ[i];
+		++i;
+	}
+	new_env[i] = entry;
+	free(g_environ);
+	g_environ = new_env;
+}
+
+void	ft_set_env(char *name, char *val, char **env)
+{
+	int		i;
+	size_t	len;
+	char *entry;
+
+	entry = ft_strjoinv(2, "=", name, val);
+	i = -1;
+	len = ft_strlen(name);
+	while (env[++i])
+	{
+		if (ft_strlen(env[i]) >= len)
+		if (!(ft_strncmp(env[i], name, len)) && env[i][len] == '=')
+		{
+			env[i] = entry;
+			return ;
+		}
+	}
+	add_env_entry(entry);
+}
